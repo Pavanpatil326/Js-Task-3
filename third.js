94d2debffea8968b7d6a8028502ae684ed843dc8
Pavanpatil326 <143817491+Pavanpatil326@users.noreply.github.com>
2024-09-05 10:23:08 +0530

    Add third.js

---
+++ third.js
@@ -0,0 +1,177 @@
+// if statement and else statement
+
+/*let price = 350
+
+if (price >= 250 )
+{
+   console.log("This is Branded")
+}
+else {
+    console.log("This is lowest")
+}
+    */
+
+
+/*let color = "yellow"
+
+if(color === "red") 
+    {
+        console.log("stop alert")
+    }
+    
+if(color ===  "green") 
+    {
+        console.log("Go")
+    }
+
+    
+if(color === "yellow") 
+    {
+        console.log("slow down")
+    }
+*/
+
+// else if statement
+/*let age = 15;
+
+if (age>=50)
+{
+    console.log("you can go to job")
+
+}
+
+else if (age>=20)
+{
+    console.log("you can go to college")
+}
+else if(age< 18)
+{
+    console.log("you can go to school")
+}
+*/
+
+// practice qs
+
+/*let  popcornsize = 35;
+
+if (popcornsize>=250)
+{
+    console.log("xl")
+}
+
+else if (popcornsize>=200)
+{
+    console.log("L")
+}
+else if (popcornsize>=100)
+    {
+        console.log("M")
+    }
+else if (popcornsize<50)
+    {
+        console.log("L")
+    }
+  */
+ 
+  // nested if else 
+  
+  /* let marks = 99;
+
+  if (marks>=33)
+  {
+    console.log("pass")
+    if(marks<=80)
+    {
+        console.log("A")
+    }
+    else{
+        console.log("O")
+    }
+  }
+  else {
+    console.log("Beter Luck Next Time")
+  }
+  */  
+
+  // logical operators
+
+  /*let marks = 40;
+
+  if (marks >= 33 && marks < 80 || !false   )
+  {
+    console.log("Pass")
+    console.log("A+")
+  }
+    */
+
+  // truthy and falsy
+
+  /* if(0){
+    console.log("This Is True Value")
+  }
+  else{
+    console.log("This Is False Value ")
+  }
+
+  */
+ 
+  // switch statement
+
+ /* let color ="Green";
+
+  switch(color) {
+    case "red" :
+        console.log("Stop Alert!");
+    case "Yellow" :
+         console.log("Slow Down");
+    case "Green" :
+        console.log("Go");
+    default :
+        console.log("Broken Light");        
+  }
+  */
+ 
+  // practice qs
+
+  /* let days = 15;
+
+  switch(days){
+    case 1 :
+        console.log("Monday");
+        break;
+    case 2 :
+         console.log("Tuesday");
+         break;
+    case 3 :
+        console.log("Wednesday");
+        break;
+    case 4 :
+        console.log("Thersday");
+        break;
+    case 5 :
+        console.log("Friday");
+        break;
+    case 6 :
+        console.log("Saturday");
+        break;
+    case 7 :
+        console.log("Sanday");    
+        break;
+    default :
+        console.log("This IS Week");    
+  }
+
+  */
+
+  // Alert and Prompts
+
+  console.log("This is error msg");
+  console.error("This is error msg");
+  console.warn("This is warning msg");
+
+let firstname =  prompt("Enter Your First Name");
+let lastname =  prompt("Enter Your Last Name")
+let mse = "Welcome" +""+ firstname +""+ lastname +""+ "!";
+  alert(mse)
+
+ 
